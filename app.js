8b073f35b35a42bb5bb2c10dc2c1a65c56d14add
Isaac <isaac.rosado95@gmail.com>
2016-06-15 16:11:11 -0400

    Nation scores update on quarterly reports

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var numberOfGroups = 0;
 var numberOfTeams = 0;
 var world = 0;
 var roundNumber = 1;
+var quarter = 0;
 //decide whether a round is over based on the timer or once all players have made a decision
 //either timer or waitForPlayers
 var decisionMode = "waitForPlayers";
@@ -21,8 +22,6 @@ var decidedPlayers = 0;
 var playerScores = {};
 var groupScores = {};
 var teamScores = {};
-var playerToGroup = {};
-var groupToTeam = {};
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
@@ -41,6 +40,7 @@ io.sockets.on('connection', function(socket) {
 	socket.id = playerNumber;
 	SOCKET_LIST[socket.id] = socket;
 	console.log('connection made');
+	console.log(socket.id);
 	
 	playerNumber++;
 	console.log('totalPlayers: ' + totalPlayers);
@@ -147,6 +147,7 @@ var checkPlayers = function(mode) {
 };
 
 var updateRound = function(sockets) {
+	quarterlyReport(sockets);
 	roundNumber++;
 	for(var i in sockets) {
 		var socket = sockets[i];
@@ -161,4 +162,18 @@ var updateRound = function(sockets) {
 			roundNumber: roundNumber
 		});
 	}
+};
+
+var quarterlyReport = function(sockets) {
+	if (roundNumber % 3 == 0) {
+		quarter++;
+		for (var i in sockets) {
+			var socket = sockets[i];
+			quarterTeamScores = teamScores;
+			socket.emit('quarterTeams', teamScores);
+			socket.emit('newQuarter', {
+				quarter: quarter
+			});
+		};
+	};
 };
